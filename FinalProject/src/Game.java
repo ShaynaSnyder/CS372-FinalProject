82ac6c79ced86b8d1d55dc3245afa63007411706
Shayna Snyder <ssnyder19@my.whitworth.edu>
2017-01-23 11:43:36 -0800

    pawn mobility

--- FinalProject/src/Game.java
+++ FinalProject/src/Game.java
@@ -1,52 +1,158 @@
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.*;
 import javax.swing.*;
 
-public class Game extends JComponent{
-	int correctS, correctW, correctR, pMovesX, pMovesY;
-	private JTextArea text;
-	boolean isCorrect=false;
+public class Game extends JComponent implements MouseListener{
+	Image iBoard, iSuspects1, iSuspects2, iPawn, iArrows;
+	int correctS, correctW, correctR, clickX, clickY, mouseX, mouseY;
+	int pawnX=445, pawnY=0, pMovesX=0, pMovesY=-1, guessN=0;
+	private JTextArea text, text2;
+	boolean isCorrect=false, guess1, guess2;
+	JFrame frame;
+	Suspect sGuess;
+	Weapon wGuess;
+	Room rGuess;
 	
 	Suspect[] suspects = {new Suspect("Miss Scarlet"), new Suspect("Colonel Mustard"), new Suspect("Mrs. White"), new Suspect("Mr. Green"), new Suspect("Mrs. Peacock"), new Suspect("Professor Plum"), new Suspect("Mr. Boddy")};
 	Weapon[] weapons = {new Weapon("Rope"), new Weapon("Revolver"), new Weapon("Wrench"), new Weapon("Knife"), new Weapon("Candlestick"), new Weapon("Lead Pipe")};
 	Room[] rooms = {new Room("Study"), new Room("Hall"), new Room("Lounge"), new Room("Library"), new Room("Dining Room"), new Room("Billard Room"), new Room("Conservatory"), new Room("Ball Room"), new Room("Kitchen")};
 	
-	public Game(){
+	public Game(Image iB, Image iS1, Image iS2, Image iP, Image iA){
+		guess1=false;
+		guess2=false;
+		text = new JTextArea();
+		text2 = new JTextArea();
+		text.setBounds(705, 10, 290, 100);
+		text2.setBounds(705, 110, 290, 150);
+		text.setLineWrap(true);
+		text2.setLineWrap(true);
+		text.setWrapStyleWord(true);
+		text2.setWrapStyleWord(true);
+		text.setFont(new Font("Serif",Font.BOLD,18));
+		text2.setFont(new Font("Serif",Font.PLAIN, 16));
+		add(text);
+		add(text2);
+		text.setEditable(false);
+		iBoard = iB;
+		iSuspects1 = iS1;
+		iSuspects2 = iS2;
+		iPawn = iP;
+		iArrows = iA;
+		addMouseListener(this);
+		Random rnd = new Random();
+		correctS = rnd.nextInt(7);
+		correctW = rnd.nextInt(6);
+		correctR = rnd.nextInt(9);
+		suspects[correctS].setCorrect();
+		weapons[correctW].setCorrect();
+		rooms[correctR].setCorrect();}
+	
+	public void promptGuess(Room r){
+		rGuess = r;
+		text.setText(String.format("Make a guess in the %s.", r.getName()));
+		guess1=true;}
+	
+	public void guessSuspect(Suspect s){
+		guessN+=1;
+		text2.setText(String.format("Guess #%d\nRoom: %s\nSuspect: %s", guessN, rGuess.getName(), sGuess.getName()));
+		guess2=true;
+		guess1=false;
+	}
+	
+	public void paint(Graphics g){
+		Graphics2D g2 = (Graphics2D)g;
+		g2.drawImage(iBoard, 0, 10, this);
+		g2.drawImage(iSuspects1, 5, 560, this);
+		g2.drawImage(iSuspects2, 405, 560, this);
+		g2.drawImage(iPawn, pawnX, pawnY, this);
+		g2.drawImage(iArrows, 710, 410, this);
+		if(text!=null)
+			text.repaint();
+		if(text2!=null)
+			text2.repaint();}
+	
+	public void mouseClicked(MouseEvent e){
+		clickX = e.getX();
+		clickY = e.getY();
 		
-	}
-	
-	public Suspect getSuspect(int id){
-		return suspects[id];
-	}
-	
-	public Weapon getWeapon(int id){
-		return weapons[id];
-	}
-	
-	public Room getRoom(int id){
-		return rooms[id];
-	}
-	
-	public void setCorrect(int cS, int cW, int cR){
-		correctS = cS;
-		correctW = cW;
-		correctR = cR;
-		suspects[cS].setCorrect();
-		weapons[cW].setCorrect();
-		rooms[cR].setCorrect();
-	}
-	
-	public JTextArea getText(){
-		return text;
-	}
-	
-	public void setPawnPosition(int x, int y){
-		pMovesX = x;
-		pMovesY = y;
-	}
-	
-	public void makeGuess(){
+		if(guess1==false){
+			//left, right, and down arrows
+			if(clickY>=460 && clickY<=505 && clickX<=830){
+				//right arrow
+				if(clickX>=793 && pMovesY>=0){
+					rightArrow();}
+				//down arrow
+				else if(clickX>=755 && clickX<=793){
+					downArrow();}
+				//left arrow
+				else if(clickX>712 && pMovesY>=0){
+					leftArrow();}
+				else
+					text.setText("You can't do that.");}
+			//up arrow
+			else if((clickY>=410 && clickY<=460) && (clickX>=755 && clickX<=793)){
+				upArrow();}
+			repaint();}
 		
-	}
+		//study
+		if(pMovesX<=-10 && pMovesY<=2){
+			promptGuess(rooms[0]);}
+		//hall
+		else if(pMovesX>=-7 && pMovesX<=-2 && pMovesY<=5){
+			promptGuess(rooms[1]);}
+		//lounge
+		else if(pMovesX>=1 && pMovesY<=4){
+			promptGuess(rooms[2]);}
+		
+		if(guess1==true){
+			//scarlet
+			if(clickX<100 && clickY>560){
+				sGuess = suspects[0];}
+			guessSuspect(sGuess);}
+		repaint();}
+	
+	public void leftArrow(){
+		if(pMovesX>-15){
+			pawnX-=26;
+			pMovesX-=1;
+			text.setText(null);}
+		else
+			text.setText("You can't do that.");}
+	
+	public void rightArrow(){
+		if(pMovesX<6){
+			pawnX+=26;
+			pMovesX+=1;
+			text.setText(null);}
+		else
+			text.setText("You can't do that.");}
+	
+	public void downArrow(){
+		if(pMovesY<23){
+			pawnY+=21;
+			pMovesY+=1;
+			text.setText(null);}
+		else
+			text.setText("You can't do that.");}
+	
+	public void upArrow(){
+		if(pMovesY>0){
+			pawnY-=21;
+			pMovesY-=1;
+			text.setText(null);}
+		else
+			text.setText("You can't do that.");}
+	
+	public void mouseEntered(MouseEvent e) {}
+
+	public void mouseExited(MouseEvent e) {}
+
+	public void mousePressed(MouseEvent e) {}
+
+	public void mouseReleased(MouseEvent e) {}
 }
--- FinalProject/src/GameTest.java
+++ FinalProject/src/GameTest.java
@@ -1,33 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import java.net.URL;
 
-public class GameTest extends JComponent implements MouseListener{
-	Game game = new Game();
-	JFrame frame;
-	private JTextArea text;
-	Image iBoard, iSuspects, iPawn, iArrows;
-	int correctS, correctW, correctR, clickX, clickY, mouseX, mouseY;
-	int pawnX=445, pawnY=0, pMovesX=0, pMovesY=-1;
 
+public class GameTest extends JComponent{
 	
 	public static void main(String[] args) {
-		Game game = new Game();
 		
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		try{
-			//URL board = new URL("http://www.printablee.com/postpic/2014/09/clue-board-game-rooms_259531.jpg");
 			String board = "/resources/boardgame.jpg";
 			Image iB = toolkit.getImage(GameTest.class.getResource(board));
 			iB = iB.getScaledInstance(700, 550, Image.SCALE_SMOOTH);
 			
-			URL suspects = new URL("https://s-media-cache-ak0.pinimg.com/originals/38/14/a9/3814a9faf7368fe933658e8bfa4a5a66.jpg");
-			Image iS = toolkit.getImage(suspects);
-			iS = iS.getScaledInstance(600, 300, Image.SCALE_SMOOTH);
+			String suspects1 = "/resources/suspects1.jpg";
+			Image iS1 = toolkit.getImage(GameTest.class.getResource(suspects1));
+			iS1 = iS1.getScaledInstance(400, 170, Image.SCALE_SMOOTH);
 			
-			//URL pawn = new URL("https://img.clipartfest.com/163b9a8f76858d8877a35bbc044748e9_sorry-game-piece-clip-art-game-pawn-clipart-transparent_566-800.png");
+			String suspects2 = "/resources/suspects2.jpg";
+			Image iS2 = toolkit.getImage(GameTest.class.getResource(suspects2));
+			iS2 = iS2.getScaledInstance(300, 170, Image.SCALE_SMOOTH);
+			
 			String pawn = "resources/pawn.png";
 			Image iP = toolkit.getImage(GameTest.class.getResource(pawn));
 			iP = iP.getScaledInstance(40, 50, Image.SCALE_SMOOTH);
@@ -37,100 +29,9 @@ public class GameTest extends JComponent implements MouseListener{
 			iA = iA.getScaledInstance(120, 100, Image.SCALE_SMOOTH);
 			
 			JFrame frame = new JFrame("Clue");
-			frame.getContentPane().add(new GameTest(game, iB, iS, iP, iA));
-			frame.setSize(900, 900);
+			frame.getContentPane().add(new Game(iB, iS1, iS2, iP, iA));
+			frame.setSize(1000, 800);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);}
 		catch(Exception ex){;}}
-	
-	public GameTest(Game g, Image iB, Image iS, Image iP, Image iA){
-		game = g;
-		iBoard = iB;
-		iSuspects = iS;
-		iPawn = iP;
-		iArrows = iA;
-		addMouseListener(this);
-		int cS, cW, cR;
-		Random rnd = new Random();
-		cS = rnd.nextInt(7);
-		cW = rnd.nextInt(6);
-		cR = rnd.nextInt(9);
-		game.setCorrect(cS, cW, cR);
-		}
-	
-	public void paint(Graphics g){
-		Graphics2D g2 = (Graphics2D)g;
-		g2.drawImage(iBoard, 0, 10, this);
-		g2.drawImage(iSuspects, 0, 560, this);
-		g2.drawImage(iPawn, pawnX, pawnY, this);
-		g2.drawImage(iArrows, 710, 410, this);
-		if(text!=null)
-			text.repaint();}
-	
-	public void mouseClicked(MouseEvent e){
-		clickX = e.getX();
-		clickY = e.getY();
-		text = new JTextArea();
-		text.setBounds(610, 570, 280, 280);
-		text.setLineWrap(true);
-		text.setWrapStyleWord(true);
-		text.setFont(new Font("Serif",Font.BOLD,20));
-		add(text);
-		text.setEditable(false);
-		
-	
-		//left, right, and down arrows
-		if(clickY>=460 && clickY<=505 && clickX<=830){
-			//right arrow
-			if(clickX>=793 && pMovesY>=0){
-				if(pMovesX<6){
-					pawnX+=26;
-					pMovesX+=1;}
-				else
-					text.setText("You can't do that.");}
-			//down arrow
-			else if(clickX>=755 && clickX<=793){
-				if(pMovesY<23){
-					pawnY+=21;
-					pMovesY+=1;}
-				else
-					text.setText("You can't do that.");}
-			//left arrow
-			else if(clickX>712 && pMovesY>=0){
-				if(pMovesX>-15){
-					pawnX-=26;
-					pMovesX-=1;}
-				else
-					text.setText("You can't do that.");}
-			else
-				text.setText("You can't do that.");}
-		//up arrow
-		else if((clickY>=410 && clickY<=460) && (clickX>=755 && clickX<=793)){
-			if(pMovesY>0){
-				pawnY-=21;
-				pMovesY-=1;}
-			else
-				text.setText("You can't do that.");}
-		
-		//lounge
-		if(pMovesX>=1 && pMovesY<=4){
-			text.setText("Make a guess in the Lounge.");}
-		//hall
-		if(pMovesX>=-7 && pMovesX<=-2 && pMovesY<=5){
-			text.setText("Make a guess in the Hall.");}
-		//study
-		if(pMovesX<=-10 && pMovesY<=2){
-			text.setText("Make a guess in the Study.");}
-		
-		game.setPawnPosition(pMovesX, pMovesY);
-		repaint();
-	}
-	
-	public void mouseEntered(MouseEvent e) {}
-
-	public void mouseExited(MouseEvent e) {}
-
-	public void mousePressed(MouseEvent e) {}
-
-	public void mouseReleased(MouseEvent e) {}
 }
